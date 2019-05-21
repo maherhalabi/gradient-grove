9abd075c5266cced319751f60e5b3d1981aa50aa
mohur <m.ashrahfi@gmail.com>
2019-05-20 23:15:36 -0700

    Adding functional choices for gradient styles

--- script.js
+++ script.js
@@ -59,3 +59,33 @@ colorButton.addEventListener("click", function() {
 gradientButton.addEventListener("click", function() {
     menuListToggle(dropDownGradientMenu);
 });
+
+var gradientStyleOne = document.getElementById('gradientOne');
+
+gradientStyleOne.addEventListener("click", function() {
+    displayHexColorInfo();
+    body.style.background = "linear-gradient(to right, " + firstColor.value + ", " + secondColor.value + ")";
+});
+
+var gradientStyleTwo = document.getElementById('gradientTwo');
+
+gradientStyleTwo.addEventListener("click", function() {
+    displayHexColorInfo();
+    body.style.background = "linear-gradient(to left, " + firstColor.value + ", " + secondColor.value + ")";
+});
+    
+var gradientStyleThree = document.getElementById('gradientThree');
+
+gradientStyleThree.addEventListener("click", function() {
+    displayHexColorInfo();
+    body.style.background = "linear-gradient(to bottom, " + firstColor.value + ", " + secondColor.value + ")";
+});
+    
+var gradientStyleFour = document.getElementById('gradientFour');
+
+gradientStyleFour.addEventListener("click", function() {
+    displayHexColorInfo();
+    body.style.background = "linear-gradient(to top, " + firstColor.value + ", " + secondColor.value + ")";
+});
+    
+
