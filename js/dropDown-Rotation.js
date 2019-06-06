43b8a2a5b584966a207fe2b7f94890e57b682bbb
mohur <m.ashrahfi@gmail.com>
2019-06-06 14:01:21 -0700

    Fixing styles with Astaz Osman

--- js/dropDown-Rotation.js
+++ js/dropDown-Rotation.js
@@ -1,6 +1,8 @@
 var dragDropGradient = document.querySelector("#rotateGradient");
 var dropDownRotation = document.querySelector(".dropDownRotation");
+var rotationIcon = document.querySelector(".fa-undo");
 
 dragDropGradient.addEventListener("click", function() {
     menuListToggle(dropDownRotation);
+    menuListToggle(rotationIcon);
 });
